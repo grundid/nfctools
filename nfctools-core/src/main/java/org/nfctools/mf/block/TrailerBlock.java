587fa7fd21bed369d7977c0278205dc3fb84d4a8
Adrian Stabiszewski <as@grundid.de>
2012-03-29 14:24:25 +0200

    added license

--- nfctools-core/src/main/java/org/nfctools/mf/block/TrailerBlock.java
+++ nfctools-core/src/main/java/org/nfctools/mf/block/TrailerBlock.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2011 Adrian Stabiszewski, as@nfctools.org
+ * Copyright 2011-2012 Adrian Stabiszewski, as@nfctools.org
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.nfctools.mf.block;
 
 import org.nfctools.mf.Key;
