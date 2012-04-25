ac436f67afac76d8cfe416884952d513653d04a9
Adrian Stabiszewski <as@grundid.de>
2012-04-25 12:47:50 +0200

    added license header

--- nfctools-ndef/src/main/java/org/nfctools/ndef/auri/AbsoluteUriRecord.java
+++ nfctools-ndef/src/main/java/org/nfctools/ndef/auri/AbsoluteUriRecord.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2011 Adrian Stabiszewski, as@nfctools.org
+ * Copyright 2011-2012 Adrian Stabiszewski, as@nfctools.org
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,9 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
  */
-
 package org.nfctools.ndef.auri;
 
 import org.nfctools.ndef.Record;
